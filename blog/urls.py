622825cb04adc9cc058e0aa254f682f6d597a0be
amirashoori7 <a.ashoori7@gmail.com>
2019-11-16 11:10:35 +0330

    RSS Feed added

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path('contact/', views.contact_us, name='contact_us'),
     path('<slug:post>/', views.post_detail, name='post_detail'),
     path('tag/<slug:tag_slug>/',
-         views.index, name='index')
+         views.index, name='index'),
+    path('blog/feed/', LatestPostsFeed(), name='post_feed'),
 ]
